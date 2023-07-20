c74e99ba7dbded2a49011b9c79f6600fbbbefbb3
Geovanny OG <geochoa212@gmail.com>
2023-07-19 21:50:22 -0500

    Agg animacion a el banco de fotos

--- app/src/main/java/com/example/restcardviewapi/MainFotos.java
+++ app/src/main/java/com/example/restcardviewapi/MainFotos.java
@@ -1,12 +1,15 @@
 package com.example.restcardviewapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 
 import com.example.restcardviewapi.Adaptador.AdaptadorFotos;
 
@@ -21,6 +24,11 @@ public class MainFotos extends AppCompatActivity {
         RecyclerView rc = (RecyclerView) findViewById(R.id.recviewfotos);
         rc.setHasFixedSize(true);
         rc.setLayoutManager(new GridLayoutManager(this,2));
+        rc.setItemAnimator(new DefaultItemAnimator());
+        int resId = R.anim.layout_animation_down_to_up;
+        LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(getApplicationContext(),
+                resId);
+        rc.setLayoutAnimation(animation);
         AdaptadorFotos adapcell = new AdaptadorFotos(this, bnd.getStringArrayList("imgs"));
         rc.setAdapter(adapcell);
     }
